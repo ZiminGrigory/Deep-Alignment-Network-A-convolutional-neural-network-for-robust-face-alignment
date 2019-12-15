34229e337fa207df38955bc434393ed65d93c7e9
Grigorii Zimin <gzimin@luxoft.com>
2019-12-15 17:21:28 +0300

    add more functions (+some fixes)

--- DAN_V2/My_DAN_V2.py
+++ DAN_V2/My_DAN_V2.py
@@ -87,7 +87,7 @@ def storeFramesWithMarks(generator):
 
         cv2.imwrite("/home/greg/dev/csc_practice_autumn2019/300W_HELEN/TST/{:05d}.png".format(j), outImg)
 
-def storeFramesWithMarksAndPredictRes(generator, predictFunc):
+def showFramesWithMarksAndPredictRes(generator, predictFunc):
     # data_dir='./data_dir'
     data_dir=""
     model_dir='dan_tf/DAN_V2/model_dir3'
@@ -115,16 +115,13 @@ def storeFramesWithMarksAndPredictRes(generator, predictFunc):
 
 
     predict_results = estimator.predict(generator)
-    j = 0
     for x in predict_results:
-        j+=1
         landmark = x['s2_ret']
         img = x['img']
-        outImg = np.zeros([112,112,3])
-        outImg[:,:,0] = np.squeeze(img, axis=2)
-        outImg[:,:,1] = np.squeeze(img, axis=2)
-        outImg[:,:,2] = np.squeeze(img, axis=2)
-        print (img.shape)
+        outImg = np.zeros([140,112,3])
+        outImg[:112,:,0] = np.squeeze(img, axis=2)
+        outImg[:112,:,1] = np.squeeze(img, axis=2)
+        outImg[:112,:,2] = np.squeeze(img, axis=2)
         for x,y in landmark:
             outImg[111 if int(y) > 110  else int(y),111  if int(x) > 110 else int(x),:] = [0,0, 255]
 
@@ -132,15 +129,13 @@ def storeFramesWithMarksAndPredictRes(generator, predictFunc):
         eyes2 = [item for sublist in eyes for item in sublist]
         prediction = predictFunc(eyes2)
         if prediction == 1:
-            outImg[:,:,1] = 127
+            outImg[112:,:,1] = 127
         else:
-            outImg[:,:,0] = 127
-
+            outImg[112:,:,0] = 127
 
         cv2.imshow('video with bboxes', cv2.resize(outImg,(300,300)).astype(np.uint8))
         if cv2.waitKey(33) == 27: 
             break  # esc to quit
-        # cv2.imwrite("/home/greg/dev/csc_practice_autumn2019/300W_HELEN/TST/{:05d}_{}.png".format(j, prediction), outImg)
 
 def storeEyesData(generator, fileName):
     # data_dir='./data_dir'
@@ -177,3 +172,54 @@ def storeEyesData(generator, fileName):
             eyes2 = [item for sublist in eyes for item in sublist]
             writer.writerow(eyes2)
 
+def storeEyesDataAndImages(generator, folderForResults):
+    # data_dir='./data_dir'
+    data_dir=""
+    model_dir='dan_tf/DAN_V2/model_dir3'
+    data_format='channels_last'
+    batch_size=64
+    mode = tf.estimator.ModeKeys.PREDICT
+
+    flags = {"data_dir":data_dir, "model_dir": model_dir, "data_format":data_format,
+        "dan_stage": 2, "num_lmark": 68, "batch_size": batch_size, "mode":mode}
+
+    def vgg16_model_fn(features, labels, mode, params):
+        return my_dan_run_loop.dan_model_fn(features=features,
+                            groundtruth=labels,
+                            mode=mode,
+                            stage=2,
+                            num_lmark=68,
+                            model_class=VGG16Model,
+                            mean_shape=None,
+                            imgs_mean=None,
+                            imgs_std=None,
+                            data_format='channels_last')
+
+    estimator = my_dan_run_loop.dan_main(flags, vgg16_model_fn)
+
+
+
+    resultsCSV = folderForResults + 'features.csv'
+    with open(resultsCSV, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+
+        predict_results = estimator.predict(generator)
+        j = 0
+        for x in predict_results:
+            j = j+1
+            landmark = x['s2_ret']
+            eyes = landmark[36:48]
+            eyes2 = [item for sublist in eyes for item in sublist]
+            writer.writerow(eyes2)
+            predict_results = estimator.predict(generator)
+
+            #images
+            img = x['img']
+            outImg = np.zeros([112,112,3])
+            outImg[:,:,0] = np.squeeze(img, axis=2)
+            outImg[:,:,1] = np.squeeze(img, axis=2)
+            outImg[:,:,2] = np.squeeze(img, axis=2)
+            for x,y in landmark:
+                outImg[111 if int(y) > 110  else int(y),111  if int(x) > 110 else int(x),:] = [0,0, 255]
+
+            cv2.imwrite("{}{:05d}.png".format(folderForResults, j), outImg)
